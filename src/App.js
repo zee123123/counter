21534adf74ee8f9563f70bb75f8473f0a233df3a
Ze <louisliu000123@gmail.com>
2021-11-16 14:54:34 -0330

    This is a studying project for React.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './component/Button';
+import Button from './component/Button';
+import './component/Display';
+import Display from './component/Display';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlusHandler = (c) => () => setCount(count + c);
+
+  const onClickMinusHandler = () => {
+    setCount(count - 1);
+  };
+
+  const onClickResetHandler = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Display count={count} />
+        <Button text={'+1'} onClick={onClickPlusHandler(1)} />
+        <Button text={'RESET'} onClick={onClickResetHandler} />
+        <Button text={'-1'} onClick={onClickMinusHandler} />
+        <Button text={'+10'} onClick={onClickPlusHandler(10)} />
       </header>
     </div>
   );
---
+++ src/component/Display.js
@@ -0,0 +1,9 @@
+const Display = ({ count }) => {
+  return (
+    <>
+      <h1>{count}</h1>
+    </>
+  );
+};
+
+export default Display;
